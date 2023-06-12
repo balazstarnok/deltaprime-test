159613095b303973545dae4ad11c9c5b58a1c397
balazstarnok <tbalazs0205@gmail.com>
2023-06-12 08:03:22 +0200

    Update index.mjs

--- lambdaScheduler/index.mjs
+++ lambdaScheduler/index.mjs
@@ -1,8 +1,37 @@
-export const handler = async(event) => {
-    // TODO implement
-    const response = {
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+const AWS = require('aws-sdk');
+const lambda = new AWS.Lambda({region: "eu-west-3"})
+
+async function invokeUpdateStatusLambda(){
+    return await new Promise((resolve, reject) => {
+        const params = {
+            FunctionName: "periodicUpdatePrimeAccountsStatus",
+            Payload: JSON.stringify({
+            })
+        }
+
+        lambda.invoke(params, (err, results) => {
+            if (err) reject(err);
+            else resolve(results);
+        })
+    })
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+export const handler = async (event) => {
+    console.log(`Going to invoke updateStatus lambda ${process.env.numberOfInvocations} times every ${process.env.timeInbetweenInvocations} ms`)
+    for(let i=0; i< process.env.numberOfInvocations; i++){
+        invokeUpdateStatusLambda();
+        console.log('Invoked updateStatus lambda')
+        console.log(`Sleeping ${process.env.timeInbetweenInvocations} ms.`)
+        await sleep(process.env.timeInbetweenInvocations)
+    }
+    return {
         statusCode: 200,
-        body: JSON.stringify('NA Szia from Lambda!'),
+        body: JSON.stringify(`Scheduler ended succesfully`),
     };
-    return response;
 };
