916ddb37d3be6313d4e1eb4741a7fb7779f73527
balazstarnok <tbalazs0205@gmail.com>
2023-06-13 18:16:50 +0200

    Delete manageProtocol directory

--- manageProtocol/index.mjs
+++
@@ -1,8 +0,0 @@
-export const handler = async(event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Olle from Lambda!'),
-    };
-    return response;
-};
